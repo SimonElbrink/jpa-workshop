74495f3dc184262cab286d27d6a249fe3d2cc3b0
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-05-27 09:43:32 +0200

    init commit

---
+++ src/test/java/se/lexicon/simon/JPAworkshop/JpaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.simon.JPAworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
